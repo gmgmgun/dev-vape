420f4dac0bbf2e6aaeba2c090a931efb5a424142
dongminlee <gmgmgun@gmail.com>
2024-04-22 01:50:30 +0900

    Chore: 불필요한 주석 제거

--- src/components/form/SignUpForm.tsx
+++ src/components/form/SignUpForm.tsx
@@ -5,47 +5,6 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { IFormData, inputs } from './formConfig';
 
-// export interface FormData {
-//   email: string;
-//   password: string;
-//   passwordConfirm?: string;
-//   nickname?: string;
-// }
-
-// interface InputProps {
-//   id: keyof FormData;
-//   type: string;
-//   placeholder: string;
-//   validation: Record<string, string>;
-// }
-
-// const inputs: InputProps[] = [
-//   {
-//     id: 'email',
-//     type: 'email',
-//     placeholder: '이메일',
-//     validation: { required: '이메일은 필수 항목입니다.' },
-//   },
-//   {
-//     id: 'password',
-//     type: 'password',
-//     placeholder: '비밀번호',
-//     validation: { required: '비밀번호는 필수 항목입니다.' },
-//   },
-//   {
-//     id: 'passwordConfirm',
-//     type: 'password',
-//     placeholder: '비밀번호 확인',
-//     validation: { required: '비밀번호 확인은 필수 항목입니다.' },
-//   },
-//   {
-//     id: 'nickname',
-//     type: 'text',
-//     placeholder: '닉네임',
-//     validation: { required: '닉네임은 필수 항목입니다.' },
-//   },
-// ];
-
 interface SignUpFormProps {
   signUp: (data: IFormData) => Promise<void>;
 }
