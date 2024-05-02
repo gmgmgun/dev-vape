b45759e1136a89a408e0cd6b87725c1edba25ce2
dongminlee <gmgmgun@gmail.com>
2024-05-02 19:59:30 +0900

    Feat: fetchProduct 컴포넌트 추가

---
+++ src/queries/fetchProduct.tsx
@@ -0,0 +1,37 @@
+import { db } from '@/firebase';
+import { ProductWithId } from '@/types/Product';
+import { doc, getDoc } from 'firebase/firestore';
+
+export default async function fetchProduct({
+  queryKey,
+}: {
+  queryKey: unknown[];
+}) {
+  const [_key, productId] = queryKey as [string, string];
+
+  try {
+    if (productId !== undefined) {
+      const docRef = doc(db, _key, productId);
+      const docSnapshot = await getDoc(docRef);
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        const product: ProductWithId = {
+          docId: docSnapshot.id,
+          id: data.id,
+          sellerId: data.sellerId,
+          productName: data.productName,
+          productPrice: data.productPrice,
+          productQuantity: data.productQuantity,
+          productDescription: data.productDescription,
+          productCategory: data.productCategory,
+          productImage: data.productImage,
+          createdAt: data.createdAt,
+          updatedAt: data.updatedAt,
+        };
+        return product;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
